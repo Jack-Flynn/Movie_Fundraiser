6916c57c2563518d5dedff6683c5fc1377e0abc2
Jack-Flynn <flynn.jack@awatapu.school.nz>
2022-05-09 15:05:26 +1200

    name and age needs to break when lower than 12

---
+++ Name and Age.py
@@ -0,0 +1,24 @@
+def int_check(question, low_num, high_num):
+
+    error = "Please enter a whole number between {} and {}".format(low_num, high_num)
+
+    valid = False
+    while not valid:
+
+        try:
+            response = int(input(question))
+
+            if low_num <= response <= high_num:
+                return response
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+
+name = input("What is your name?")
+
+age = int_check("How old are you?", 12, 125)
+
+
