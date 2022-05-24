5270549455f2c04ca3acfc1a5c56aa90adf18ffd
Jack-Flynn <flynn.jack@awatapu.school.nz>
2022-05-25 09:57:55 +1200

    tickets pretty much done?

--- Tickets.py
+++ Tickets.py
@@ -1,7 +1,5 @@
 def age_check(question, low_num, high_num):
-
     error = "Please enter a whole number between {} and {}".format(low_num, high_num)
-
     valid = False
     while not valid:
 
@@ -15,10 +13,10 @@ def age_check(question, low_num, high_num):
                 if try_again in yes:
                     print(error)
                 elif try_again in no:
-                    break
+                    exit()
                 else:
                     print("I'll assume that means no")
-                    break
+                    exit()
         except ValueError:
             print(error)
 
@@ -30,3 +28,11 @@ ticket_prices = ["7.50", "10.50", "6.50"]
 
 age = age_check("How old are you?", 12, 125)
 
+if age >= 12 <= 15:
+    tickets = ticket_prices[0]
+elif age >= 16 <= 64:
+    tickets = ticket_prices[1]
+else:
+    tickets = ticket_prices[2]
+
+print("if you are {} then that will cost {}".format(age, tickets))
