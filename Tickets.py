7a02bd1d1f631b010302d1646a156087b1445bd2
Jack-Flynn <flynn.jack@awatapu.school.nz>
2022-05-26 10:58:18 +1200

    tickets function needs to be corrected

--- Tickets.py
+++ Tickets.py
@@ -21,6 +21,24 @@ def age_check(question, low_num, high_num):
             print(error)
 
 
+def ticket_price():
+    error = "please enter yes or no"
+    valid = False
+    while not valid:
+        ticket_question = input("do you want tickets?").lower()
+        if ticket_question in yes:
+            if 12 <= age <= 15:
+                tickets = ticket_prices[0]
+            elif 16 <= age <= 64:
+                tickets = ticket_prices[1]
+            else:
+                tickets = ticket_prices[2]
+        elif ticket_question in no:
+            exit()
+        else:
+            print(error)
+
+
 yes = ["yes", "y", "yep", "accept", "yeah", "yea"]
 no = ["no", "n", "nope", "nah", "deny", "nay"]
 ticket_prices = ["7.50", "10.50", "6.50"]
@@ -28,11 +46,5 @@ ticket_prices = ["7.50", "10.50", "6.50"]
 
 age = age_check("How old are you?", 12, 125)
 
-if age >= 12 <= 15:
-    tickets = ticket_prices[0]
-elif age >= 16 <= 64:
-    tickets = ticket_prices[1]
-else:
-    tickets = ticket_prices[2]
 
-print("if you are {} then that will cost {}".format(age, tickets))
+print("if you are {} then that will cost ${}".format(age, tickets))
