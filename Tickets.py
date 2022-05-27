ddf3ff9a1a10f7ba9708287875ea9d8f124a7ae7
Jack-Flynn <flynn.jack@awatapu.school.nz>
2022-05-27 12:15:22 +1200

    tickets still broken

--- Tickets.py
+++ Tickets.py
@@ -21,30 +21,33 @@ def age_check(question, low_num, high_num):
             print(error)
 
 
-def ticket_price():
+def ticket_calculation(question):
     error = "please enter yes or no"
     valid = False
+    response = input(question).lower()
     while not valid:
-        ticket_question = input("do you want tickets?").lower()
-        if ticket_question in yes:
-            if 12 <= age <= 15:
-                tickets = ticket_prices[0]
-            elif 16 <= age <= 64:
-                tickets = ticket_prices[1]
-            else:
-                tickets = ticket_prices[2]
-        elif ticket_question in no:
-            exit()
-        else:
+        try:
+            if question in yes:
+                if 12 <= age <= 15:
+                    tickets = ticket_prices[0]
+                elif 16 <= age <= 64:
+                    tickets = ticket_prices[1]
+                else:
+                    tickets = ticket_prices[2]
+                return response
+            elif question in no:
+                exit()
+        except ValueError:
             print(error)
 
 
 yes = ["yes", "y", "yep", "accept", "yeah", "yea"]
 no = ["no", "n", "nope", "nah", "deny", "nay"]
-ticket_prices = ["7.50", "10.50", "6.50"]
+ticket_prices = [7.50, 10.5, 6.5]
+seat_count = 150
+while seat_count > 0:
+    age = age_check("How old are you?", 12, 125)
+    ticket_calculation("do you want tickets?")
 
-
-age = age_check("How old are you?", 12, 125)
-
-
-print("if you are {} then that will cost ${}".format(age, tickets))
+    print("if you are {} then that will cost ${:.2f}".format(age, tickets))
+    seat_count -= 1
